952005b1db955992463e0a422feab2dcdad27f14
Daniel <daniel.lub2007@gmail.com>
2024-08-23 23:39:03 +0300

    Commit 2

---
+++ app/src/main/java/com/example/restaurantapp/Main.java
@@ -0,0 +1,61 @@
+package com.example.restaurantapp;
+
+import android.content.ClipData;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.restaurantapp.databinding.ActivityMainBinding;
+public class Main extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) ->
+        {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Intent intent = getIntent();
+        replaceFragment(new GmapsFragment());
+        binding.bottomNavMenu.setOnItemSelectedListener(item ->
+        {
+            Fragment fragment = null;
+            int itemId = item.getItemId();
+            if (itemId == R.id.Gmaps)
+            {
+                replaceFragment(new GmapsFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        binding = null;
+    }
+}
