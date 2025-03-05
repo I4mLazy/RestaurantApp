ca7dca7eb9e26fdef48ed8f7001716f528a229a4
I4mLazy <daniel.lub2007@gmail.com>
2025-03-05 22:57:37 +0200

    Commit 05/3/2025
    Created beginning of Profile screen.
    -Profile
    -Edit Profile
    -Settings
    -Logout button.
    
    In edit profile added option to change profile picture based on camera or gallery. Does not save to firebase currently.
    
    REMEMBER: Make backstacks work. DOES NOT WORK CURRENTLY

---
+++ app/src/main/java/com/example/restaurantapp/fragments/SettingsFragment.java
@@ -0,0 +1,25 @@
+package com.example.restaurantapp.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.restaurantapp.R;
+
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the settings layout
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
